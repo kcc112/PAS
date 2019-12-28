6d49b2f8e85f16be5229ae4af536dd4034986414
Kamil Celejewski <kamil12283@gmail.com>
2019-12-28 22:13:17 +0100

    [Add] simple api

---
+++ src/main/java/com/pas/pas/api/v1/DevelopersApi.java
@@ -0,0 +1,36 @@
+package com.pas.pas.api.v1;
+
+import com.pas.pas.model.developers.Developer;
+import com.pas.pas.service.DeveloperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/developers")
+@RestController
+public class DevelopersApi {
+
+    private final DeveloperService developerService;
+
+    @Autowired
+    public DevelopersApi(DeveloperService developerService) {
+        this.developerService = developerService;
+    }
+
+    @GetMapping
+    public List<Developer> getAllDevelopers()  {
+        return developerService.getAllDevelopers();
+    }
+
+    @GetMapping(path = "{id}")
+    public Developer getDevelopersByName(@PathVariable UUID id) {
+        return developerService.selectDeveloperById(id).orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteDeveloper(@PathVariable UUID id) {
+        developerService.destroyDeveloper(id);
+    }
+}
